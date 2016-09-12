eddd88ee52f97607ab8a1b1d4889a57447873166
Josh LaBounty <joshua.labounty@stonybrook.edu>
2016-09-11 22:15:37 -0400

    Adds fit function

--- Compton/FitGaussian.C
+++ Compton/FitGaussian.C
@@ -11,9 +11,9 @@ int FitGaussian()
 	hraw->Draw();
 
 	//Open file and get the attached histogram for the background
-	std::string file_name = "Cs_137_Weak_090716_141039.root";
+	std::string file_name_back = "Cs_137_Weak_090716_141039.root";
 	//std::string file_name = "Co_60_Weak_090716_140110.root";
-	TFile *fback = new TFile(("./data/"+file_name).c_str());
+	TFile *fback = new TFile(("./data/"+file_name_back).c_str());
 	TH1F *hback = new TH1F();
 	hback = (TH1F*)f->Get("h");
 	TCanvas *c2 = new TCanvas();
@@ -22,8 +22,12 @@ int FitGaussian()
 	//Create a new histogram which is the data w/o background
 	TCanvas *c3 = new TCanvas();
 	TH1F *h = (TH1F*)h->Clone("hraw");
-	h->Add(hback,-1);
+//	h->Add(hback,-1);
 	h->Draw();
+
+	//Fit the histogram to a gaussian
+        TF1 *fit1 = new TF1("fit1","gaus",650,2000);
+        h->Fit("fit1","R");
 
 	
 
