5bfad5a6b278a91ea462d0fe682eaea850d04c9b
tkrahulik <thomas.krahulik@stonybrook.edu>
2016-12-04 19:31:22 -0500

    Plotting and report updates

--- PhaseTransitions/makePlot_VvT_BaTiO3.C
+++ PhaseTransitions/makePlot_VvT_BaTiO3.C
@@ -1,6 +1,8 @@
 int makePlot_VvT_BaTiO3(
 			TString infile1 = "data/mV_vs_T111416_Run2.txt",
-			TString infile2 = "data/BaTiO3_CurieWeiss.txt"
+			TString infile2 = "data/BaTiO3_CurieWeiss.txt",
+			// TString infile3 = "data/BaTiO3_CurieWeiss_LowT.txt",
+			TString infile3 = "data/BaTiO3_CurieWeiss_LowT2.txt"
 )
 {
 
@@ -11,72 +13,148 @@ int makePlot_VvT_BaTiO3(
   vector<double> V, T, C1, C2, k;
   for(int i = 0; i<n; i++)
     {
+      if(TData->GetV2()[i] > 50.)
+	{
       V.push_back(TData->GetV1()[i]);
       T.push_back(TData->GetV2()[i]);
 
-      C1.push_back(TMath::Abs(V[i]/1.99) / (2*TMath::Pi()*25000.*1029));
-      C2.push_back(1.99*(1 - sqrt(1 - 4*(V[i]/1.99)**2. )) / (2*TMath::Pi()*25000.*1029*2*TMath::Abs(V[i])));
+      C1.push_back(TMath::Abs(TData->GetV1()[i]/1.99) / (2*TMath::Pi()*25000.*1029));
+      // C2.push_back((10**12.)*1.99*(1 - sqrt(1 - 4*(V[i]/1.99)**2. )) / (2*TMath::Pi()*25000.*1029*2*TMath::Abs(V[i])));
+      C2.push_back((10**12.)*1.99*(1 - sqrt(1 - 4*(TData->GetV1()[i]/1.99)**2. )) / (2*TMath::Pi()*25000.*1029*2*TMath::Abs(TData->GetV1()[i])));
 
-      // k.push_back( C2[i]/ (8.854*(10**(-12.)) ));
+      // k.push_back( C2[i] / (3.2384*10**(-13.)));
 
-      k.push_back( C2[i] / (3.2384*10**(-13.)));
+      k.push_back((10**12.)*1.99*(1 - sqrt(1 - 4*(TData->GetV1()[i]/1.99)**2. )) / (2*TMath::Pi()*25000.*1029*2*TMath::Abs(TData->GetV1()[i])) / (3.2384*10**(-13.)));
 
+	}
     }
 
   TTree *TDataCW = new TTree();
   TDataCW->ReadFile(infile2, "TCW/D:VCW:nullCW");
   int nCW = TDataCW->Draw("VCW:TCW", "", "goff");
   vector<double> VCW, TCW, C1CW, C2CW, kCW;
-  for(int iCW = 0; iCW<n; iCW++)
+  for(int iCW = 0; iCW<nCW; iCW++)
     {
+      if(TDataCW->GetV2()[iCW] > 50.)
+	{
       VCW.push_back(TDataCW->GetV1()[iCW]);
       TCW.push_back(TDataCW->GetV2()[iCW]);
 
-      C1CW.push_back(TMath::Abs(VCW[iCW]/1.99) / (2*TMath::Pi()*25000.*1029));
-      C2CW.push_back(1.99*(1 - sqrt(1 - 4*(VCW[iCW]/1.99)**2. )) / (2*TMath::Pi()*25000.*1029*2*TMath::Abs(VCW[iCW])));
+      C1CW.push_back(TMath::Abs(TDataCW->GetV1()[iCW]/1.99) / (2*TMath::Pi()*25000.*1029));
+      C2CW.push_back((10**12.)*1.99*(1 - sqrt(1 - 4*(TDataCW->GetV1()[iCW]/1.99)**2. )) / (2*TMath::Pi()*25000.*1029*2*TMath::Abs(TDataCW->GetV1()[iCW])));
 
-      // k.push_back( C2[i]/ (8.854*(10**(-12.)) ));
 
-      kCW.push_back( C2CW[iCW] / (3.2384*10**(-13.)));
+      // kCW.push_back( C2CW[iCW] / (3.2384*10**(-13.)));
+      kCW.push_back((10**12.)*1.99*(1 - sqrt(1 - 4*(TDataCW->GetV1()[iCW]/1.99)**2. )) / (2*TMath::Pi()*25000.*1029*2*TMath::Abs(TDataCW->GetV1()[iCW])) / (3.2384*10**(-13.)));
 
+	}
+    }
+
+  TTree *TDataLow = new TTree();
+  TDataLow->ReadFile(infile3, "TLow/D:VLow:nullLow");
+  int nLow = TDataLow->Draw("VLow:TLow", "", "goff");
+  vector<double> VLow, TLow, C1Low, C2Low, kLow;
+  for(int iLow = 0; iLow<nLow; iLow++)
+    {
+      if(TDataLow->GetV2()[iLow] > 50.)
+	{
+      VLow.push_back(TDataLow->GetV1()[iLow]);
+      TLow.push_back(TDataLow->GetV2()[iLow]);
+
+      C1Low.push_back(TMath::Abs(TDataLow->GetV1()[iLow]/1.99) / (2*TMath::Pi()*25000.*1029));
+      C2Low.push_back((10**12.)*1.99*(1 - sqrt(1 - 4*(TDataLow->GetV1()[iLow]/1.99)**2. )) / (2*TMath::Pi()*25000.*1029*2*TMath::Abs(TDataLow->GetV1()[iLow])));
+
+
+      // kLow.push_back( C2Low[iLow] / (3.2384*10**(-13.)));
+      kLow.push_back((10**12.)*1.99*(1 - sqrt(1 - 4*(TDataLow->GetV1()[iLow]/1.99)**2. )) / (2*TMath::Pi()*25000.*1029*2*TMath::Abs(TDataLow->GetV1()[iLow])) / (3.2384*10**(-13.)));
+
+	}
     }
 
 
   TCanvas *c1 = new TCanvas();  
-  TGraph *g_VvT = new TGraph(n, &T[0], &V[0]);
+  TGraph *g_VvT = new TGraph(T.size(), &T[0], &V[0]);
   g_VvT->SetTitle("");
-  g_VvT->GetXaxis()->SetTitle("T ( ^{#circ}C)");
+  g_VvT->GetXaxis()->SetTitle("T (#circC)");
   g_VvT->Draw("AP");
 
-  TGraph *g_VvT_CW = new TGraph(nCW, &TCW[0], &VCW[0]);
-  g_VvT_CW->SetLineColor(kRed);
-  g_VvT_CW->Draw("same");
+  // TGraph *g_VvT_CW = new TGraph(TCW.size(), &TCW[0], &VCW[0]);
+  // g_VvT_CW->SetLineColor(kRed);
+  // g_VvT_CW->Draw("same");
 
   // TCanvas *c2 = new TCanvas();
   // TGraph *g_C1vT = new TGraph(n, &T[0], &C1[0]);
   // g_C1vT->Draw("AP");
 
   TCanvas *c3 = new TCanvas();  
-  TGraph *g_C2vT = new TGraph(n, &T[0], &C2[0]);
+  TGraph *g_C2vT = new TGraph(T.size(), &T[0], &C2[0]);
   g_C2vT->SetTitle("");
-  g_C2vT->GetXaxis()->SetTitle("T ( ^{#circ}C)");
+  g_C2vT->GetXaxis()->SetTitle("T (#circC)");
+  g_C2vT->GetYaxis()->SetTitle("C (pF)");
+  // double Cb = g_C2vT->Eval(126.2394);
+  double Cb = g_C2vT->Eval(126.2394);
+  cout << Cb << endl;
+
   g_C2vT->Draw("AP");
-  TGraph *g_C2vT_CW = new TGraph(nCW, &TCW[0], &C2CW[0]);
-  g_C2vT_CW->SetLineColor(kRed);
-  g_C2vT_CW->Draw("same");
+
+  TLine *Tc = new TLine(126.2, 340., 126.2, 540.);
+  Tc->SetLineStyle(2);
+  Tc->SetLineColor(kRed);
+  Tc->Draw();
+
+  TLegend *leg = new TLegend(0.2, 0.6, 0.5, 0.7);
+  leg->SetNColumns(1);
+  leg->AddEntry(g_C2vT, "BaTiO_{3} C vs T", "p");
+  leg->AddEntry(Tc, "T_{C} = 126.2 #circC", "l");
+  leg->Draw();
+
+  TGraph *g_C2vT_CW = new TGraph(TCW.size(), &TCW[0], &C2CW[0]);
+  // g_C2vT_CW->SetLineColor(kRed);
+  // g_C2vT_CW->Draw("same");
+  TGraph *g_C2vT_Low = new TGraph(TLow.size(), &TLow[0], &C2Low[0]);
+  // g_C2vT_Low->SetLineColor(kRed);
+  // g_C2vT_Low->Draw("same");
 
 
-  TCanvas *c4 = new TCanvas();  
-  TGraph *g_kvT = new TGraph(n, &T[0], &k[0]);
-  g_kvT->SetTitle("");
-  g_kvT->GetXaxis()->SetTitle("T ( ^{#circ}C)");
-  g_kvT->Draw("AP");
-  TGraph *g_kvT_CW = new TGraph(nCW, &TCW[0], &kCW[0]);
-  g_kvT_CW->SetLineColor(kRed);
-  g_kvT_CW->Draw("same");
+  TCanvas *c4 = new TCanvas();
+  TH1 *h4 = c4->DrawFrame(30., 0., 150., 5000.);
+  h4->SetTitle("");   
+  h4->GetXaxis()->SetTitle("T (#circC)");
+  h4->GetYaxis()->SetTitle("k ");
+  TGraph *g_kvT = new TGraph(T.size(), &T[0], &k[0]);
 
-  TF1* fit = new TF1("fit", "[0]/(x-[1])", 120., 150.);
-  g_kvT_CW->Fit("fit");
+  for(int iG = 0; iG < g_C2vT->GetN(); iG++)
+    {
+      g_kvT->SetPoint(iG, g_C2vT->GetX()[iG], (g_C2vT->GetY()[iG]*Cb / (Cb - g_C2vT->GetY()[iG])) / (3.2384*10**(-13)));
+    }
+
+  g_kvT->Draw("P");
+  TGraph *g_kvT_CW = new TGraph(TCW.size(), &TCW[0], &kCW[0]);
+  for(int iGCW = 0; iGCW < g_C2vT_CW->GetN(); iGCW++)
+    {
+      g_kvT_CW->SetPoint(iGCW, g_C2vT_CW->GetX()[iGCW], (g_C2vT_CW->GetY()[iGCW]*Cb / (Cb - g_C2vT_CW->GetY()[iGCW])) / (3.2384*10**(-13)));
+    }
+  g_kvT_CW->SetMarkerColor(kRed);
+  g_kvT_CW->Draw("same, P");
+
+  TGraph *g_kvT_Low = new TGraph(TLow.size(), &TLow[0], &kLow[0]);
+  for(int iGLow = 0; iGLow < g_C2vT_Low->GetN(); iGLow++)
+    {
+      g_kvT_Low->SetPoint(iGLow, g_C2vT_Low->GetX()[iGLow], (g_C2vT_Low->GetY()[iGLow]*Cb / (Cb - g_C2vT_Low->GetY()[iGLow])) / (3.2384*10**(-13)));
+    }
+  g_kvT_Low->SetMarkerColor(kBlue);
+  g_kvT_Low->Draw("same, P");
+
+  TF1* fit = new TF1("fit", "[0]/(x-[1])", 126., 150.);
+  // fit->SetParameters(500., 126.2);
+  g_kvT_CW->Fit("fit", "","", 130., 142.);
+
+  TF1* fit2 = new TF1("fit2", "[0]/([1]-x)", 90., 115.);
+  fit2->SetParameters(500., 122.);
+  g_kvT_Low->Fit("fit2", "","", 90., 115.);
+  g_kvT_Low->GetFunction("fit2")->SetLineColor(kBlue);
+
+  // c3->Print("report/plots/BaTiO3_CvT_withTC.png");
 
   return 0;
 }
--- PhaseTransitions/makePlot_VvT_VO2.C
+++ PhaseTransitions/makePlot_VvT_VO2.C
@@ -1,5 +1,6 @@
 int makePlot_VvT_VO2(
-			TString infile = "data/VO2_V_vs_T_111616.txt"
+		     TString infile = "data/VO2_V_vs_T_111616.txt",
+		     TString infile2 = "data/VO2_EnergyGap.txt"
 )
 {
 
@@ -7,7 +8,7 @@ int makePlot_VvT_VO2(
   TData->ReadFile(infile, "T/D:V:null");
   int n = TData->Draw("V:T", "", "goff");
 
-  vector<double> V, T, R, C1, C2, k;
+  vector<double> V, T, R, pLOG, TInv;
   for(int i = 0; i<n; i++)
     {
       V.push_back(TData->GetV1()[i]);
@@ -16,12 +17,26 @@ int makePlot_VvT_VO2(
       // R.push_back(V[i]*(9970000. + 1029) / (1.99 - V[i]));
       R.push_back(V[i]*(997000. + 1029) / (1.99 - V[i]));
 
-      C1.push_back(TMath::Abs(V[i]/1.99) / (2*TMath::Pi()*25000.*1029));
-      C2.push_back(1.99*(1 - sqrt(1 - 4*(V[i]/1.99)**2. )) / (2*TMath::Pi()*25000.*1029*2*TMath::Abs(V[i])));
+      pLOG.push_back(TMath::Log(R[i]));
+      TInv.push_back(1 / T[i]);
 
-      // k.push_back( C2[i]/ (8.854*(10**(-12.)) ));
+    }
 
-      k.push_back( C2[i] / (3.2384*10**(-13.)));
+  TTree *TDataEGap = new TTree();
+  TDataEGap->ReadFile(infile2, "TEGap/D:VEGap:nullEGap");
+  int nEGap = TDataEGap->Draw("VEGap:TEGap", "", "goff");
+
+  vector<double> VEGap, TEGap, REGap, pLOGEGap, TInvEGap;
+  for(int iEGap = 0; iEGap<nEGap; iEGap++)
+    {
+      VEGap.push_back(TDataEGap->GetV1()[iEGap]);
+      TEGap.push_back(TDataEGap->GetV2()[iEGap]);
+
+      // REGap.push_back(VEGap[iEGap]*(9970000. + 1029) / (1.99 - VEGap[iEGap]));
+      REGap.push_back(VEGap[iEGap]*(997000. + 1029) / (1.99 - VEGap[iEGap]));
+
+      pLOGEGap.push_back(TMath::Log(REGap[iEGap]));
+      TInvEGap.push_back(1 / TEGap[iEGap]);
 
     }
 
@@ -68,9 +83,45 @@ int makePlot_VvT_VO2(
   leg->AddEntry(TcUP, "T_{C} #pm T*", "l");
   leg->Draw();
 
-  // TCanvas *c4 = new TCanvas();  
-  // TGraph *g_kvT = new TGraph(n, &T[0], &k[0]);
-  // g_kvT->Draw("AP");
+  /*=====================*/
+  TCanvas *c4 = new TCanvas();
+  // c4->SetLogy();
+  TGraph *g_RvT2 = new TGraph(n, &TInv[0], &pLOG[0]);
+  g_RvT2->SetTitle("");
+  g_RvT2->Draw("AP");
+  TGraph *g_RvTEGap2 = new TGraph(nEGap, &TInvEGap[0], &pLOGEGap[0]);
+  g_RvTEGap2->SetMarkerColor(kRed);
+  g_RvTEGap2->Draw("SAME, P");
+
+  g_RvTEGap2->Fit("pol1");
+
+
+  TLegend *leg2 = new TLegend(0.55, 0.2, 0.8, 0.35);
+  leg2->SetNColumns(1);
+  leg2->AddEntry(g_RvT, "VO_{2} R vs T", "p");
+  leg2->AddEntry(g_RvTEGap2->GetFunction("pol1"), "#splitline{Semiconductor}{Resistivity Law}", "l");
+  leg2->AddEntry((TObject*)0 ,"E_{G} = ", "");
+  leg2->Draw();
+  /*=====================*/
+
+  TCanvas *c5 = new TCanvas();
+  c5->SetLogy();
+  g_RvT->Draw("AP");
+  TGraph *g_RvTEGap = new TGraph(nEGap, &TEGap[0], &REGap[0]);
+  g_RvTEGap->SetMarkerColor(kRed);
+  g_RvTEGap->Draw("SAME, P");
+
+  // g_RvTEGap->Fit("expo");
+
+
+  // TLegend *leg2 = new TLegend(0.2, 0.2, 0.45, 0.35);
+  // leg2->SetNColumns(1);
+  // leg2->AddEntry(g_RvT, "VO_{2} R vs T", "p");
+  // leg2->AddEntry(g_RvTEGap->GetFunction("expo"), "#splitline{Semiconductor}{Resistivity Law}", "l");
+  // leg2->AddEntry((TObject*)0 ,"E_{G}", "");
+  // leg2->Draw();
+
+
 
   // c3->Print("report/plots/VO2_RvT_withTC.png");
 
